b1892666d836596181f99f273a22173cd078a7ea
Jimmy Edberg <oathmountain@hotmail.com>
2018-07-18 09:57:17 +0200

    added the rest

--- code.js
+++ code.js
@@ -3,28 +3,35 @@ againdiv.classList.add("hidden");
 let guessdiv = document.getElementById("guess");
 let feedback = document.getElementById("tries");
 
-let tryCount = 4;
-let theValue = (Math.random()*10)+1;
+let value = -1;
+let tryCount = 0;
+let theValue = 0
 startNewGame();
-reduceTries();
 
 let tryButton = document.getElementById("enterGuessBtn");
 let playAgainButton = document.getElementById("playAgainBtn");
 
 let listener = function () {
     let field = document.getElementById("input");
-    let value = parseInt(field.value);
+    value = parseInt(field.value);
     field.value = "";
-    checkNumber(value);
+    checkNumber();
 }
 let resetListener = function() {
     startNewGame();
+    toggleHidden();
 }
 tryButton.addEventListener("click", listener);
 playAgainButton.addEventListener("click",resetListener)
 
+function startNewGame() {
+    value = -1;
+    tryCount = 4;
+    theValue = Math.floor(Math.random()*10)+1;
+    reduceTries();
+}
 
-function checkNumber(value) {
+function checkNumber() {
     if (value !== theValue){
         reduceTries();
         if(tryCount === 0){
@@ -33,13 +40,18 @@ function checkNumber(value) {
         }
     } else {
         feedback.innerText = "You wonnered"
+        toggleHidden();
     }
 }
 
 function reduceTries() {
     let tries = document.getElementById("tries");
-    tries.innerText = "You have " + (--tryCount) + " tries left";
-    return tryCount;
+    tries.innerText = "You have " + (--tryCount) + " tries left.";
+    if (value < theValue && value !== -1) {
+        tries.innerText += " Try higher!!"
+    } else if (value > theValue && value !== -1) {
+        tries.innerText += " Try lower!!"
+    }
 }
 function toggleHidden() {
     againdiv.classList.toggle("hidden");
