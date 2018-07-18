6b697edad57e92e5234bce4062c6d31d4417d0d7
Jimmy Edberg <oathmountain@hotmail.com>
2018-07-18 09:33:07 +0200

    added toggle for class hidden and some nmore stuff

--- code.js
+++ code.js
@@ -1,26 +1,38 @@
 let againdiv = document.getElementById("playagain");
 againdiv.classList.add("hidden");
+let guessdiv = document.getElementById("guess");
+let feedback = document.getElementById("tries");
 
 let tryCount = 4;
 let theValue = (Math.random()*10)+1;
+startNewGame();
 reduceTries();
 
 let tryButton = document.getElementById("enterGuessBtn");
+let playAgainButton = document.getElementById("playAgainBtn");
 
 let listener = function () {
     let field = document.getElementById("input");
-    field.value = "";
     let value = parseInt(field.value);
+    field.value = "";
     checkNumber(value);
 }
+let resetListener = function() {
+    startNewGame();
+}
 tryButton.addEventListener("click", listener);
+playAgainButton.addEventListener("click",resetListener)
+
 
 function checkNumber(value) {
     if (value !== theValue){
         reduceTries();
         if(tryCount === 0){
-            console.log("say goodbye");
+            toggleHidden();
+            feedback.innerText = "You didnt wonnered"
         }
+    } else {
+        feedback.innerText = "You wonnered"
     }
 }
 
@@ -28,4 +40,8 @@ function reduceTries() {
     let tries = document.getElementById("tries");
     tries.innerText = "You have " + (--tryCount) + " tries left";
     return tryCount;
+}
+function toggleHidden() {
+    againdiv.classList.toggle("hidden");
+    guessdiv.classList.toggle("hidden");
 }
